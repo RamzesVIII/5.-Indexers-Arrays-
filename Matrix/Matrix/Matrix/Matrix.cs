43880d8fdc97216b009b865e56371abc3a7eaa61
Roman <litl_rom@tut.by>
2019-11-01 21:22:36 +0300

    matrix 60%

---
+++ Matrix/Matrix/Matrix/Matrix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    class Matrix
+    {
+        static int row = Convert.ToInt32(Console.ReadLine());
+        static int col = Convert.ToInt32(Console.ReadLine());
+        Random rand = new Random();
+
+        int[,] matrix = new int[row,col];
+
+        public Matrix ()
+        {
+            
+        }
+
+        public void MakeMatrix ()
+        {
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    matrix[j, i] = rand.Next(1, 100);
+                }
+            }
+        }
+
+        public void ShowMatrix ()
+        {
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    Console.Write(matrix[j,i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Matrix/Matrix/Matrix/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix mymatrix = new Matrix();
+            mymatrix.MakeMatrix();
+            mymatrix.ShowMatrix();
+        }
+    }
+}
