afd568e107b7e8d523d2a3ecb6753dd6755ac3b3
Roman <litl_rom@tut.by>
2019-11-03 19:16:03 +0300

    first store commit

---
+++ Store/Store/Store/Article.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    class Article
+    {
+        private string article;
+        private string store;
+        private double price;
+
+        
+        public Article (string article, string store, double price)
+        {
+            this.article = article;
+            this.store = store;
+            this.price = price;
+        }
+    }
+}
---
+++ Store/Store/Store/Store.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    class Store
+    {
+        private Article[] mystore = new Article[5];
+
+        public Store ()
+        {
+            mystore[0] = new Article("notebook", "AMD", 1500);
+            mystore[1] = new Article("tv", "lg", 1200);
+            mystore[2] = new Article("phone", "samsung", 800);
+            mystore[3] = new Article("mouse", "logitech", 30);
+            mystore[4] = new Article("monitor", "lg", 300);
+        }
+
+        public Article this [int index]
+        {
+            get { return mystore[index]; }
+            set {
+
+                mystore[index] = value;
+                }
+        }
+    }
+}
